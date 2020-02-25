9995f8c69a0e3af2817424a685ca7bee41ec882a
Dmitry Maltsev <mosfett@yandex.ru>
2020-02-25 15:39:22 +0300

    little fix

--- src/main/java/org/epam/oop/home/appliance/Supervisor/KitchenMatching.java
+++ src/main/java/org/epam/oop/home/appliance/Supervisor/KitchenMatching.java
@@ -1,5 +1,6 @@
 package org.epam.oop.home.appliance.Supervisor;
 
+import org.epam.oop.home.appliance.applianceType.TypeOfDevice;
 import org.epam.oop.home.appliance.defenitionApliance.Appliance;
 import static org.epam.oop.home.appliance.applianceType.TypeOfDevice.MICROWAVE;
 import static org.epam.oop.home.appliance.applianceType.TypeOfDevice.STOVE;
@@ -8,12 +9,13 @@ public class KitchenMatching {
 
     public static void kitchenMatching(Appliance[] dev){
 
+//        for(Appliance dev1:dev){
+//            if (dev1.typeOfDevice ==
+//        }
             for (int i = 0; i < dev.length; i++) {
-        if (dev[i].typeOfDevice != MICROWAVE
-                || dev[i].typeOfDevice != STOVE )
-
+        if (!(dev[i].typeOfDevice == MICROWAVE
+                || dev[i].typeOfDevice == STOVE ))
                 {
-            System.out.println(dev[i].typeOfDevice.name());
             dev[i].setKitchenAppliance(false);
         }
 
